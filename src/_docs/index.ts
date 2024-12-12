c095a1d2e7f3d4354b719838a5e69bb79b768c85
Takuto Yanagida <tokutadaginaya@gmail.com>
2024-12-12 22:55:34 +0900

    Improve to show the result of the color adjustment

--- src/_docs/index.ts
+++ src/_docs/index.ts
@@ -2,32 +2,38 @@
  * Script for Sample
  *
  * @author Takuto Yanagida
- * @version 2024-12-10
+ * @version 2024-12-12
  */
 
 import 'klales/klales.min.css';
-import { Color, ColorSpace } from 'iroay/iroay';
+import { Color } from 'iroay/iroay';
 import { Scheme, Adjuster, Parameters, SolverType } from '../../cosaf';
 
-type Triplet = [number, number, number];
-
 document.addEventListener('DOMContentLoaded', (): void => {
-	const cs = [
+	const s = new Scheme([
 		Color.fromString('#f00')?.asInteger() as number,
 		Color.fromString('#090')?.asInteger() as number,
 		Color.fromString('#091')?.asInteger() as number,
-	];
+	]);
+
 	const p = new Parameters();
 	p.setRatioModeEnabled(false);
-	p.setTargetDesirability(0.8);
+	// p.setHuePreserved(false);
+	// p.setTonePreserved(false);
+	p.setTargetDesirability(null);
 	p.setSolver(SolverType.FC);
 	// p.setSolver(SolverType.FUZZY_BREAKOUT);
 	// p.setSolver(SolverType.SRS3);
-	const s = new Scheme(cs);
 	const a = new Adjuster(p);
+
+	a.addListener((s: Scheme, degree: number): boolean => {
+		console.log(degree.toFixed(2), '--', s.toString());
+		return 0.8 < degree;
+	});
 	const mod: Scheme | null = a.adjust(s);
 	if (mod) {
-		console.log(s.toString());
-		console.log(mod.toString());
+		for (const v of mod) {
+			console.log(v.getColor().toString());
+		}
 	}
 });
