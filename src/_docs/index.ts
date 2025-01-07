9e99e524c9c97367620cc2819fd50c8ba8fd9c3f
Takuto Yanagida <tokutadaginaya@gmail.com>
2025-01-07 22:51:29 +0900

    Minor change

--- src/_docs/index.ts
+++ src/_docs/index.ts
@@ -2,7 +2,7 @@
  * Script for Sample
  *
  * @author Takuto Yanagida
- * @version 2024-12-12
+ * @version 2025-01-07
  */
 
 import 'klales/klales.min.css';
@@ -10,30 +10,30 @@ import { Color } from 'iroay/iroay';
 import { Scheme, Adjuster, Parameters, SolverType } from '../../cosaf';
 
 document.addEventListener('DOMContentLoaded', (): void => {
-	const s = new Scheme([
+	const orig = new Scheme([
 		Color.fromString('#f00')?.asInteger() as number,
 		Color.fromString('#090')?.asInteger() as number,
-		Color.fromString('#091')?.asInteger() as number,
+		Color.fromString('#009')?.asInteger() as number,
 	]);
+	log(orig);
 
 	const p = new Parameters();
-	p.setRatioModeEnabled(false);
+	// p.setRatioModeEnabled(false);
 	// p.setHuePreserved(false);
 	// p.setTonePreserved(false);
-	p.setTargetDesirability(null);
+
 	p.setSolver(SolverType.FC);
 	// p.setSolver(SolverType.FUZZY_BREAKOUT);
 	// p.setSolver(SolverType.SRS3);
-	const a = new Adjuster(p);
 
-	a.addListener((s: Scheme, degree: number): boolean => {
-		console.log(degree.toFixed(2), '--', s.toString());
-		return 0.8 < degree;
-	});
-	const mod: Scheme | null = a.adjust(s);
-	if (mod) {
-		for (const v of mod) {
-			console.log(v.getColor().toString());
-		}
-	}
+	const a = new Adjuster(p);
+	const mod: Scheme | null = a.adjust(orig);
+	if (mod) log(mod);
 });
+
+function log(s: Scheme): void {
+	for (const v of s) {
+		console.log(v.getColor().toString());
+	}
+	console.log(s.toString());
+}
